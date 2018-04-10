a7eead5a9fab896716d54a3334937dd79bc09640
Oniya Daniel <daniel.prince84@gmail.com>
2018-04-10 07:37:01 +0100

    can now create a new entry

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -2,7 +2,23 @@
 module.exports = (app, db) => {
 
     app.post("/note", (req, res) => {
+        let note = {
+            title: req.body.title,
+            message: req.body.message,
+        };
 
-        res.send("You should create a Note!");
+        db.collection("notes").insert(note, (err, result) => {
+
+            if (err) {
+                console.log(err);
+                res.send({ error: "An error occured. Could no store your note"});
+            }
+            else {
+
+                console.log(result);
+                res.send(result.ops[0]);
+            }
+
+        });
     });
 };
--- server.js
+++ server.js
@@ -2,14 +2,27 @@
 const express = require("express")
 const MongoClient = require("mongodb").MongoClient;
 const BodyParser = require("body-parser");
+const mongoDb = require("./config/db").mongodb;
 
 const app = express();
-const routes = require("./app/routes")(app, {});
 
 app.use( BodyParser.urlencoded({extended: true}) );
 
 const port = 8002;
 
-app.listen(port, () => {
-    console.log("We are live");
-});
+MongoClient.connect(mongoDb.url, (err, database) => {
+
+    if (err) {
+
+        return console.log("database connection error! :(");
+    }
+
+    let DB = database.db("open-diary");
+
+    const routes = require("./app/routes")(app, DB);
+
+    app.listen(port, () => {
+        console.log(">> Server Started");
+    });
+
+})
